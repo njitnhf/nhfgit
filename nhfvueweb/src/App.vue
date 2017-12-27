02bb71aeccc3b8edb360c58ac556f9985051f7ea
NHF-VSC <njitnhf@163.com>
2017-12-27 17:56:14 +0800

    27日练习

--- nhfvueweb/src/App.vue
+++ nhfvueweb/src/App.vue
@@ -1,37 +1,39 @@
 <template>
-  　　<div id="app">
-  　　　　<img src="./assets/logo.png">
-　　　　  <h2>{{msg}}</h2>
-　　　　  <firstcomponent></firstcomponent>
-          <router-link to="/view1">Go to view1</router-link>
-          <router-link to="/view2">Go to view2</router-link>
-          <!-- 路由出口 -->
-          <!-- 路由匹配到的组件将渲染在这里 -->
-          <router-view></router-view>
-  　　</div>
-  </template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h2>{{msg}}</h2>
+    <firstcomponent></firstcomponent>
+    <router-link to="/view1">Go to view1</router-link>
+    <router-link to="/view2">Go to view2</router-link>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
+</template>
 
 <script>
   import firstcomponent from './components/first-component.vue'
-
+  import formatDate from './date.js'
   export default {
-　　name: 'app',
-　　data () {
-　　　　return {
-　　　　　　msg: 'Welcome to Your Vue.js App'
-　　　　}
-　　},
-　　components: { firstcomponent }
-}
+    name: 'app',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    components: { firstcomponent }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Arial, Helvetica, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
+    /*chrome,safari字体抗锯齿渲染*/
+    -webkit-font-smoothing: antialiased;
+    /*firefox字体抗锯齿渲染*/
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- nhfvueweb/src/components/first-component.vue
+++ nhfvueweb/src/components/first-component.vue
@@ -1,19 +1,23 @@
 <template>
-    　　<div id="firstcomponent">
-    　　　　<h1>20171226</h1>
-    　　　　<a> 作者：{{ author }} </a>
-    　　</div>
-    </template>
-     
-    <script type="text/javascript">
-    　　export default {
-    　　　　data () {
-    　　　　　　return {
-    　　　　　　　　author: "nhf"
-    　　　　　　}
-    　　　　}
-    　　}
-    </script>
-     
-    <style>
-    </style>
+    <div id="firstcomponent">
+        <h1>{{ systemtime }}</h1>
+        <a> written by:{{ author }} </a>
+    </div>
+</template>
+
+<script type="text/javascript">
+
+    export default {
+        data() {
+            return {
+                author: "NHF",
+                systemtime: Date.now()
+
+            }
+        },
+
+    }
+</script>
+
+<style>
+</style>
